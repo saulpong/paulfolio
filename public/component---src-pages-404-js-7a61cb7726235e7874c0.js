444290c515f9cd9bf60f94f212c249b760fd7f46
taylour <trm@inspire.ca>
2019-12-31 00:35:32 -0800

    add public folder

---
+++ public/component---src-pages-404-js-7a61cb7726235e7874c0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{289:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(288),o=n(287);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7a61cb7726235e7874c0.js.map
