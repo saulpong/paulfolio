9a577848b3f67005711616520ab477eb0104671c
taylour <trm@inspire.ca>
2020-01-01 16:47:30 -0800

    Update config and public files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pathPrefix: `/gatsby-creative`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Paul Song`,
+    description: `Hi, I'm Paul. I like to make 3D Models/Music!`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ps-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
---
+++ public/styles-959cbca514ba99d76c1a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{302:function(n,o,w){},318:function(n,o,w){}}]);
+//# sourceMappingURL=styles-959cbca514ba99d76c1a.js.map
